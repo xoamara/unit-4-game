989a7bfdf91e7b0ef5633fc2bbac9f6e1b54fed8
Nixinerix <xoamara2@gmail.com>
2018-10-26 16:21:21 -0700

    Still cannot get a soft reset of the numbers figured out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,17 +6,17 @@ let targetNumber = 0;
 let counterTotal = 0;
 let wins = 0;
 let losses = 0;
-let overTarget = false;
+let softReset = true;
 
 //make a reset function
 
 function gameReset() {
     targetNumber = "";
     counterTotal = "";
-    wins = "";
-    losses = "";
+    wins = 0;
+    losses = 0;
 
-    $("#targetNumber", "#wins", "#losses", "#counter").emtpy();
+    $("#targetNumber", "#counter").emtpy();
 }
 
 // Random computer guess logic and display, between 19 and 120.
@@ -27,23 +27,15 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
 
-function softReset() {
-
-    targetNumber = getRandomIntInclusive(19, 120);
-
-    $("#targetNumber").text(targetNumber);
-
-
     for (var i = 0; i < 4; i++) {
-        var imageCrystal = $("<img>");
-        imageCrystal.addClass("crystal-image-" + i);
-        imageCrystal.attr("src", "assets/images/crystal-" + i + ".png");
+        var imageCrystal = $(".crystal-image-" +i);
         imageCrystal.attr("data-crystalvalue", getRandomIntInclusive(1, 12));
         $("#crystals").append(imageCrystal);
     }
-}
+    
+    targetNumber = getRandomIntInclusive(19, 120);
+    $("#targetNumber").text(targetNumber);
 
-softReset()
 
     $("img").on("click", function () {
         let crystalValue = ($(this).attr("data-crystalvalue"));
@@ -53,18 +45,21 @@ softReset()
 
         if (counterTotal === targetNumber) {
             wins++;
-            $("#crystals").empty();
         }
 
         if (counterTotal >= targetNumber) {
             losses++;
-            $("#crystals").empty();
         }
 
         $("#wins").text("Wins: " + wins);
         $("#losses").text("Losses: " + losses);
-
     });
+
+
+
+
+    
+
 
 
 // Wins and losses counter and display
